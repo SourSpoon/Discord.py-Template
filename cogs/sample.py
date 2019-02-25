b113ea19bff260e81614075624ef03fddd72a10e
SourSpoon <SourSpoon@users.noreply.github.com>
2019-02-25 17:17:46 +0000

    Updated this for latest version of d.py

--- cogs/sample.py
+++ cogs/sample.py
@@ -2,33 +2,34 @@ import discord
 from discord.ext import commands
 
 
-class Sample:
+class Sample(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 
+    @commands.Cog.listener()
     def on_guild_join(self, guild):
         """
-        Events in cogs don't need the decorator
+
 
         This event receives the the guild when the bot joins.
         """
         print(f'Joined {guild.name} with {guild.member_count} users!')
-    
+
     @commands.command()
     async def test(self, ctx):
         """
         A test command, Mainly used to show how commands and cogs should be laid out.
         """
         await ctx.send('Tested!')
-    
+
     @commands.group(invoke_without_command=True)
     async def foo(self, ctx):
         """
         A sub command group, Showing how sub command groups can be made.
         """
         await ctx.send('try my subcommand')
-    
+
     @foo.command(aliases=['an_alias'])
     async def bar(self, ctx):
         """
